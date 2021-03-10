b099ec7382316f80355c453b4bd4aa742bf68359
Fan Bai <bflashcp3f@gmail.com>
2021-03-09 21:15:12 -0500

    Update the data path

--- scripts/predict_xwlp_sliding.sh
+++ scripts/predict_xwlp_sliding.sh
@@ -7,7 +7,7 @@ for split_idx in 0 1 2 3 4;
 do
 
   allennlp predict ${model_dir}/xwlp-split-${split_idx}/model.tar.gz \
-    ${data_dir}/split_${split_idx}/test.json \
+    ${data_dir}/dygiepp/split_${split_idx}/test.json \
     --predictor dygie \
     --include-package dygie \
     --use-dataset-reader \
